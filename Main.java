c290c15863cf17c465db5ccd1fcbb9b1941b400c
CurlyKev <mor25730@uvg.edu.gt>
2025-09-30 07:27:16 -0600

    Nuevo item Sanacion

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Poción", 30, 0));
+        items.add(new Item("Veneno", 35, 0));
         items.add(new Item("Furia", 0, 10));
+        items.add(new Item("Sanacion", 40, 0));
 
         Scanner sc = new Scanner(System.in);
         Vista.mostrar("Elige tu rol: 1. Guerrero  2. Explorador");
